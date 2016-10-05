b03fb20394c9fa27704f87f1948e48cbc8aa0457
Moonhyok Kim <moonhyok@airbears2-10-142-52-45.airbears2.1918.berkeley.edu>
2016-10-05 15:12:44 -0700

    k

--- cafe/settings.py
+++ cafe/settings.py
@@ -126,8 +126,7 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'pcari')
 
 URL_ROOT = 'http://opinion.berkeley.edu/pcari'
-STATIC_ROOT = "/pcari/static/"
 STATIC_URL = URL_ROOT + '/static/'
