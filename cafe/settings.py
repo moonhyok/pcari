0b627ffbe4b488f60958e64d98c8df5b5b1d21f7
Moonhyok Kim <moonhyok@Moonhyoks-MacBook-Pro.local>
2016-10-04 21:49:20 -0700

    static

--- cafe/settings.py
+++ cafe/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -126,8 +126,8 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-URL_ROOT = 'http://opinion.berkeley.edu/pcari'
-STATIC_URL = URL_ROOT + '/static/'
+# URL_ROOT = 'http://opinion.berkeley.edu/pcari'
+# STATIC_URL = URL_ROOT + '/static/'
--- pcari/views.py
+++ pcari/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth import logout
 import json
 import random
 
+
 # !IMPORTANT! YOU MUST COMMENT OUT THE FOLLOWING GLOBAL VARIABLES
 # IF YOU MAKE CHANGES TO models.py
 QUAN_QUESTIONS = list(QuantitativeQuestion.objects.all())
@@ -300,6 +301,7 @@ def logout(request):
     'learn_more':TEXT['learn_more'],
     'exit':TEXT['exit']
     }
+    generate()
     return render(request, 'logout.html', context)
 
 def get_comment(request):
@@ -354,4 +356,31 @@ def update_ratings(user):
         q.save()
 
     # for q in questions:
+
+def generate():
+    ratings = Rating.objects.all()
+
+    r_count = []
+    for _ in range(11):
+        r_count.append(0)
+
+    for i in range(1,QuantitativeQuestion.objects.count()+1):
+        r = ratings.filter(qid=i)
+
+        for k in range(len(r_count)):
+            r_count[k]=0
+
+        for rating in r:
+            if r >= 0:
+                r_count[rating.score] += 1
+            elif r == -1:
+                r_count[10] += 1
+
+        with open("pcari/static/data/q%d.tsv" % i, "w") as datafile:
+            datafile.write("score   count\n")
+            for j in range(len(r_count)):
+                if j == len(r_count)-1:
+                    datafile.write("skip"+" "+str(r_count[j])+"\n")
+                else:
+                    datafile.write(str(j)+" "+str(r_count[j])+"\n")
 
