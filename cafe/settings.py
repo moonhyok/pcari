21e0d073b21942f668d5f903f28f123496fdffb2
Moonhyok Kim <moonhyok@airbears2-10-142-52-45.airbears2.1918.berkeley.edu>
2016-10-05 15:18:26 -0700

    k

--- cafe/settings.py
+++ cafe/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+# PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -126,7 +126,7 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'pcari')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'pcari')
 
 URL_ROOT = 'http://opinion.berkeley.edu/pcari'
 STATIC_URL = URL_ROOT + '/static/'
