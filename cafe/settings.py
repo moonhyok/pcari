533a54935f0f4b5dfe37a1f6f3db27e2f1f83073
Moonhyok Kim <moonhyok@Moonhyoks-MacBook-Pro.local>
2016-10-06 19:57:09 -0700

    server

--- cafe/settings.py
+++ cafe/settings.py
@@ -127,9 +127,9 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'pcari')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-# URL_ROOT = 'http://opinion.berkeley.edu/pcari'
-# STATIC_URL = URL_ROOT + '/static/'
+URL_ROOT = 'http://opinion.berkeley.edu/pcari'
+STATIC_URL = URL_ROOT + '/static/'
 
 
